223a8fea334a606a07488e008aa67b8df9797327
ismakolis <esteban.ismael.canencia@gmail.com>
2024-04-26 11:38:44 -0500

    primera clase java scrip

---
+++ javascrip/javascrip.js
@@ -0,0 +1,71 @@
+var saludo="Hola mundo dsesde JavaScrip"
+console.log(saludo)
+
+//comnetarios
+/*
+Aqui va
+
+nuestros comentarios
+
+Forma de escribir variables, la sintaxis .
+
+var nombre
+var $nombre
+var _nombre
+var nombre1
+var nombre_estudiante
+
+como no se puede escribir una variable
+var 8nombre
+tampoco se puede utilizar
+palabras reservadas en javaScrip
+var if, while, case, for
+
+ ¿Como crear archivos js?
+ nunca se usa nom,bres de archivos en mayusculas
+ JAVASCRIPT.js
+ var holamundo
+ def HolaMundo{}
+*/
+/*---------Variables en java scrip-----------
+var=>Es una variable global puede ser utilizada
+toda la vida del programa.
+
+let=> Es una variable probada y se utiliza en una parte
+especiica del programa eso quiere decir que es unas
+*/
+/*
+{
+    let hello="HELLO Quity"
+    
+}*/
+var a
+a=7
+console.log("El valor de a es ",a)
+
+let b
+b=6
+console.log("El valor de b es: ",b)
+
+console.log(window)
+console.log(window.a)
+console.log(window.b)
+
+/*variable tipo var */
+var musica="Baladas "
+console.log("VAriable antes deñ bloque",musica)
+{
+    var musica="Cumbia"
+    console.log("Variable dentro del bloque",musica)
+}
+console.log("Variable fuera del bloque",musica)
+/*Variabel tipo let */
+console.log("*********Let**********")
+let musica2="Salsa"
+console.log("VAriable antes deñ bloque",musica2)
+{
+    let musica2="Merenge"
+    console.log("Variable dentro del bloque",musica2)
+}
+console.log("Variable fuera del bloque",musica2)
+
